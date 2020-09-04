4ad52432221e8e8b603e13b65302dfbd96af8891
vtorres96 <victorcdc96@gmail.com>
2020-09-03 22:02:21 -0300

    testando revert

--- aula01/helloworld.js
+++ aula01/helloworld.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 
 
 console.log("vou mostrar para a galera o git reset");
-=======
-console.log("Alô mãe, estou ao vivo");
->>>>>>> parent of 93351d5... alterando helloworld.js e criando teste.js

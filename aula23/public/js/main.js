e566cf966ad39f4537dd3fac745193527cc28288
vtorres96 <victorcdc96@gmail.com>
2021-01-12 12:11:32 -0300

    adicionando pre aula

---
+++ aula23/public/js/main.js
@@ -0,0 +1,26 @@
+// verificando se o arquivo foi linkado corretamente
+// console.log('Hello World');
+
+// pegando valor do que foi digitado no prompt
+// let nome = prompt("Digite seu nome");
+// console.log(nome);
+
+// bloqueando para menores de 18 anos de forma simples
+// let resposta = confirm("Você confirma ter 18 anos ?");
+
+// if(!resposta){
+//   window.location.reload(false);
+// }
+
+
+// Mostrar que dá para selecionar o elemento utilizando as classes que ele possui e até 
+// mesmo o ID, e também, somente a classe e somente o ID
+// document.querySelector('ul.navbar-nav.mr-auto') 
+// document.querySelector('div#navbarNav') 
+// document.querySelector('.navbar-nav.mr-auto') 
+// document.querySelector('#navbarNav') 
+
+
+// desafio mudar a cor das LI depois de 15 segundos que a página for carregada
+// lembrando que não vale utilizar document.querySelector, somente o document.querySelectorAll
+
